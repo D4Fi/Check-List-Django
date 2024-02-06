ea4eef39b791e78039a0648702b69259b764bb06
01010010010101010101 <139288494+D4Fi@users.noreply.github.com>
2024-02-06 18:02:05 -0300

    Add files via upload

---
+++ js/cursojs/static/cursojs/js/index.js
@@ -0,0 +1,49 @@
+
+
+function create(){
+	let line_item = document.createElement('li')
+	line_item.classList.add('list-group-item')
+	line_item.classList.add('list-group-item-success')
+	line_item.innerHTML = document.querySelector('.line').value
+	document.querySelector('.list-group').appendChild(line_item);
+	input.value = 'lkj'
+	
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
